73b60ee83259a82ef0c2f1eeb4790bbf8b950ac2
Patricdv <patric.venturini@beatscode.com>
2021-01-16 00:54:15 +0000

    Starting web

---
+++ web/src/containers/Maps/Maps.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "YOUR_GOOGLE_API_KEY_GOES_HERE",
+})(MapContainer);
