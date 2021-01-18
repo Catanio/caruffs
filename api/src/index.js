0dd053cd0a09b5839cf533e40e6f310316b3f264
Mauricio Catanio <mauricatanio@yahoo.com.br>
2021-01-17 22:59:40 -0300

    added cors and created hotreload script to /api

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const cors = require('cors');
 const express = require ('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-
 const app = express();
-app.use(express.json());
-app.use(routes);
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
@@ -14,5 +12,9 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     useFindAndModify: false,
     useCreateIndex: true
 });
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(process.env.PORT);
