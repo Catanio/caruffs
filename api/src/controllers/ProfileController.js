9fcbf62b1b61e01cbf851e8c2067a2952777c480
Eduardo Sabino <eduardo@amo.delivery>
2021-05-08 22:43:18 -0300

    adding auto generated id

--- api/src/controllers/ProfileController.js
+++ api/src/controllers/ProfileController.js
@@ -1,42 +1,47 @@
 const Profile = require('../models/Profile');
 const SHA256 = require("crypto-js/sha256");
+const { get } = require('../models/utils/PointSchema');
 
 module.exports = {
-    async store(req, res) {
-      // '{  }' deconstructs data from req.body and puts inside the new consts
-      const { idUffs, password, name, bio, phone, mail } = req.body
+  async store(req, res) {
+    // '{  }' deconstructs data from req.body and puts inside the new consts
+    const { idUffs, password, name, bio, phone, mail } = req.body
 
-      const profile = await Profile.create({
-        idUffs,
-        password: SHA256(password),
-        name,
-        bio,
-        phone,
-        mail,
-      });
+    const profile = await Profile.create({
+      idUffs,
+      password: SHA256(password),
+      name,
+      bio,
+      phone,
+      mail,
+    });
 
-      return res.json( {
-        'create': profile,
-      });
-    },
+    return res.json( {
+      'create': profile,
+    });
+  },
 
-    async login(req, res) {
-      const { password, mail } = req.body
-      const profile = await Profile.find({ mail })
+  async login(req, res) {
+    const { password, mail } = req.body
+    const profile = await Profile.find({ mail })
 
-      if (!profile) {
-        res.status(404);
-        return res.send('Profile not found')
-      }
-      if (profile.password === SHA256(password)) {
-        delete profile.password
-        const token = jwt.sign(profile, process.env.SECRET, {
-          expiresIn: 10000
-        });
-        return res.json({ auth: true, token: token });
-      } else {
-        res.status(401);
-        return res.send('Wrong password')
-      }
+    if (!profile) {
+      res.status(404);
+      return res.send('Profile not found')
     }
+    if (profile.password === SHA256(password)) {
+      delete profile.password
+      const token = jwt.sign(profile, process.env.SECRET, {
+        expiresIn: 10000
+      });
+      return res.json({ auth: true, token: token });
+    } else {
+      res.status(401);
+      return res.send('Wrong password')
+    }
+  },
+
+  async get(req, res) {
+    
+  }
 }
--- api/src/models/Profile.js
+++ api/src/models/Profile.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
-    idUffs: String,
-
-    name: String,
-    bio: String,
-    phone: String,
-    mail: String,
+  _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      required: true,
+      auto: true,
+  },
+  idUffs: String,
+  name: String,
+  bio: String,
+  phone: String,
+  mail: String,
 });
 
 module.exports = mongoose.model('Profile', ProfileSchema);
